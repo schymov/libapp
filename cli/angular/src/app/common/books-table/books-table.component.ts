dee47d2d81be18295fdbabb3cef12eba9e9f8544
Vadim Kosenkov <kosenkov@itspartner.net>
2022-02-02 16:50:58 +0300

    fixed bookreader

--- cli/angular/src/app/common/books-table/books-table.component.ts
+++ cli/angular/src/app/common/books-table/books-table.component.ts
@@ -1,39 +1,41 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource } from "@angular/material/table";
-import { MatPaginator } from "@angular/material/paginator";
-import { MatSort } from "@angular/material/sort";
-import {UserService} from "../../services/user.service";
-import {Book} from "../../components/main/main.component";
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { UserService } from '../../services/user.service';
+import { Book } from '../../components/main/main.component';
+
+declare const google: any;
 
 @Component({
   selector: 'app-books-table',
   templateUrl: './books-table.component.html',
-  styleUrls: ['./books-table.component.scss']
+  styleUrls: ['./books-table.component.scss'],
 })
 export class BooksTableComponent implements OnInit {
+  toggleReader = true;
   @Input() userInfo: any;
   @Input() booksData!: Book[];
 
   displayedColumns: string[] = ['book', 'name', 'author', 'year', 'favorite'];
   dataSource!: MatTableDataSource<Book>;
 
-  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort!: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
 
-  constructor(private userServise: UserService) {
-  }
+  constructor(private userServise: UserService) {}
 
   ngOnInit() {
+    google.books.load();
     this.booksData.forEach((book: Book) => {
-        book.isFavorite = this.userInfo.favorites.includes(book._id);
-    })
+      book.isFavorite = this.userInfo.favorites.includes(book._id);
+    });
     this.dataSource = new MatTableDataSource(this.booksData);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
   applyFilter(filterValue: any) {
     this.dataSource.filter = filterValue.value.trim().toLowerCase();
-
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
@@ -41,14 +43,29 @@ export class BooksTableComponent implements OnInit {
   getOpacity(isFavourite: boolean) {
     return isFavourite ? 1 : 0.2;
   }
-  favoriteClick(id: string) {
-    this.userServise.changeUserFavorite(id).subscribe(res => {
-    });
+  favoriteClick(id: string, e: Event) {
+    e.stopPropagation();
+    this.userServise.changeUserFavorite(id).subscribe((res) => {});
     this.booksData = this.booksData.map((book: Book) => {
       if (book._id === id) {
         book.isFavorite = !book.isFavorite;
       }
       return book;
-    })
+    });
+  }
+
+  initialize(id: any) {
+    this.openReader();
+    const reader = document.getElementById('viewerCanvas');
+    const viewer = new google.books.DefaultViewer(reader);
+    viewer.load(id);
+  }
+
+  openReader() {
+    this.toggleReader = !this.toggleReader;
+  }
+
+  closeReader() {
+    this.toggleReader = !this.toggleReader;
   }
 }
--- cli/angular/src/app/components/main/main.component.ts
+++ cli/angular/src/app/components/main/main.component.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { BooksService } from "../../services/books.service";
-import { UserService } from "../../services/user.service";
+import { BooksService } from '../../services/books.service';
+import { UserService } from '../../services/user.service';
 import { forkJoin } from 'rxjs';
 
 export interface Book {
-  name: string,
-  author: string,
-  year: string,
-  img: string,
-  id: string,
-  _id: string,
-  isActive: boolean,
-  isFavorite: boolean,
+  name: string;
+  author: string;
+  year: string;
+  img: string;
+  id: string;
+  _id: string;
+  isActive: boolean;
+  isFavorite: boolean;
 }
-
-declare const google: any;
 
 @Component({
   selector: 'app-main',
@@ -22,39 +20,27 @@ declare const google: any;
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-
   booksData!: Book[];
   userInfo: any;
   isDataAvailable: boolean = false;
 
-  constructor(private booksService: BooksService, private userServise: UserService) {
-  }
+  constructor(
+    private booksService: BooksService,
+    private userServise: UserService
+  ) {}
 
   ngOnInit(): void {
     this.getData();
-    //test opening bookreader
-    //this.bookreader('F1QDAAAAQAAJ');
   }
 
   getData() {
     forkJoin(
       this.booksService.getAllBooks(),
-      this.userServise.getUserInfo(),
+      this.userServise.getUserInfo()
     ).subscribe(([resultBooks, resultUser]) => {
       this.booksData = resultBooks;
       this.userInfo = resultUser;
       this.isDataAvailable = true;
-    })
-  }
-  bookreader(id: string) {
-    google.books.load();
-
-    function initialize(id: string) {
-      const viewer = new google.books.DefaultViewer(
-        document.getElementById('viewerCanvas')
-      );
-      viewer.load(id);
-    }
-    google.books.setOnLoadCallback(() => initialize(id));
+    });
   }
 }
